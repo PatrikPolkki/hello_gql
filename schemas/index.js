b77f21d1f1c01f2dd36bebced49afc94f67919a1
patep <patrik.polkki@metropolia.fi>
2022-03-30 10:43:59 +0300

    first working graphql versio

---
+++ schemas/index.js
@@ -0,0 +1,14 @@
+// schemas/index.js
+import animalSchema from "./animalSchema";
+import { gql } from "apollo-server-express";
+
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+  }
+  type Mutation {
+    _: Boolean
+  }
+`;
+
+export default [linkSchema, animalSchema];
